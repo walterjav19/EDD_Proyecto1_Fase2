649455aa927cc16b37063abcd554c501215963a3
Walter Javier <walterjav19@gmail.com>
2023-03-22 20:51:02 -0600

    arreglando login

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.validateForm =function() {
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "admin") {
         alert("Inicio de sesión exitoso");//dashboard admin
-        window.location.replace("/Admin_Dashboard/admin.html");
+        window.location.replace("./Admin_Dashboard/admin.html");
     } else {
         alert("El nombre de usuario o la contraseña son incorrectos");//dashboard usuario   
     }
