13a83092d82a19e74bad64cca0ff28f10d28247e
Walter Javier <walterjav19@gmail.com>
2023-04-02 20:45:18 -0600

    eliminar y bitacora actualizado

--- User_Dashboard/CrearCarpeta.js
+++ User_Dashboard/CrearCarpeta.js
@@ -6,10 +6,6 @@ document.getElementById("btncrear").onclick=function (){
     $("#miModal").modal("show");
 }
 
-document.getElementById("cerrar").onclick=function (){
-    $("#miModal").modal("hide"); // Oculta la modal
-}
-
 document.getElementById("cerrar2").onclick=function (){
     $("#miModal").modal("hide"); // Oculta la modal
 }
@@ -57,3 +53,47 @@ document.getElementById("miBotonModal").onclick=function (){
     $("#miModal").modal("hide"); // Oculta la modal
 }
 
+document.getElementById("btnSi").onclick=function (){
+    let texto=document.getElementById("basic-url").value
+    let ruta="/"+texto
+    let username=JSON.parse(localStorage.getItem("estudiante")).carnet
+    const urls=JSON.parse(localStorage.getItem("urls"+username))
+
+
+    let newArbol=new Arbol()
+
+    for(let url of urls){
+        newArbol.insertar(url)
+    }
+
+    newArbol.eliminar(ruta)
+    
+    let indice=urls.indexOf(ruta);
+
+    urls.splice(indice, 1);
+    
+    localStorage.setItem("arboln"+username,JSON.stringify(newArbol))
+    localStorage.setItem("urls"+username,JSON.stringify(urls))
+
+    let fecha=new Date();
+    let actual="Fecha: "+fecha.getDate()+"-"+(fecha.getMonth()+1)+"-"+fecha.getFullYear()
+    let hora="Hora: "+fecha.getHours()+":"+fecha.getMinutes();
+    let log="Accion: se elimino carpeta \\n \\\""+ruta+"\\\" \\n"+actual+"\\n"+hora;
+
+    const listaGlobal=JSON.parse(localStorage.getItem("bitacora"+username))
+    let listanueva=new CircularLinkedList()
+    let puntero=listaGlobal.head
+    
+    do{
+       listanueva.append(puntero.data)
+       puntero=puntero.next 
+    }while(puntero!==null)
+
+    listanueva.append(log)
+    
+    localStorage.setItem("bitacora"+username,JSON.stringify(listanueva))
+
+
+    console.log(ruta)
+    $("#exampleModal").modal("hide"); // Oculta la modal
+}
